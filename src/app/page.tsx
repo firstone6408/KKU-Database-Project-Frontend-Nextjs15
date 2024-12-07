133cf40fdeabfc278bd455f15d5fbeb1f2431733
FirstOne <prosterz171@gmail.com>
2024-12-07 22:02:06 +0700

    ปิดฉาก Order, Product, Stock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+
+
 import Image from "next/image";
 
-export default function Home() {
+async function fetchApi()
+{
+  const resposne = await fetch("http://localhost:5000/api/");
+  return await resposne.json() as {
+    message: string
+  };
+}
+
+export default async function Home()
+{
+  const messageFromElysia = await fetchApi();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -21,6 +33,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>{messageFromElysia.message}.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
